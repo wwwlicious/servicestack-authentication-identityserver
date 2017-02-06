1ac614affffda156e0006b6a66a0f5660774d3b0
stuartbfs <stuart@badfeelingstudios.com>
2017-02-06 21:10:55 +0000

    Applied code cleanup

--- samples/UserAuthProvider.ServiceStack.Core.SelfHost/ServiceModel/Hello.cs
+++ samples/UserAuthProvider.ServiceStack.Core.SelfHost/ServiceModel/Hello.cs
@@ -1,4 +1,7 @@
-﻿namespace UserAuthProvider.ServiceStack.Core.SelfHost.ServiceModel
+﻿// // This Source Code Form is subject to the terms of the Mozilla Public
+// // License, v. 2.0. If a copy of the MPL was not distributed with this
+// // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+namespace UserAuthProvider.ServiceStack.Core.SelfHost.ServiceModel
 {
     using global::ServiceStack;
 
--- src/ServiceStack.Authentication.IdentityServer/Clients/DocumentDiscoveryClient.cs
+++ src/ServiceStack.Authentication.IdentityServer/Clients/DocumentDiscoveryClient.cs
@@ -1,4 +1,7 @@
-﻿namespace ServiceStack.Authentication.IdentityServer.Clients
+﻿// // This Source Code Form is subject to the terms of the Mozilla Public
+// // License, v. 2.0. If a copy of the MPL was not distributed with this
+// // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+namespace ServiceStack.Authentication.IdentityServer.Clients
 {
     using System;
     using System.Collections.Generic;
